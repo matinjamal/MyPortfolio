3867467fd94ae162a203c52576e59a831b67f0aa
matinjamal <162322083+matinjamal@users.noreply.github.com>
2024-03-13 16:34:09 +0430

    Update js.js

--- js.js
+++ js.js
@@ -1,53 +1,23 @@
-let number0 = document.getElementById("test0");
-let counter0 = 0;
-setInterval(() => {
-  if (counter0 == 2) {
-    clearInterval;
-  } else {
-    counter0 += 1;
-    number0.innerHTML = counter0 + " +";
-  }
-}, 2);
-let number = document.getElementById("test");
-let counter = 0;
-setInterval(() => {
-  if (counter == 32) {
-    clearInterval;
-  } else {
-    counter += 1;
-    number.innerHTML = counter + " +";
-  }
-}, 2);
-let number2 = document.getElementById("test1");
-let counter2 = 0;
-setInterval(() => {
-  if (counter2 == 67) {
-    clearInterval;
-  } else {
-    counter2 += 1;
-    number2.innerHTML = counter2 + " +";
-  }
-}, 2);
-let year = document.getElementById("test5");
-let counter3 = 0;
-setInterval(() => {
-  if (counter3 == 43) {
-    clearInterval;
-  } else {
-    counter3 += 1;
-    year.innerHTML = counter3 + " +";
-  }
-}, 20);
-
-
-
-
-
-
-
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerNavEl = document.querySelector(".header");
+const navHeaderEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
-  headerNavEl.classList.toggle("nav-open");
+  navHeaderEl.classList.toggle("nav-open");
+});
+
+const menu_btn = document.querySelector(".btn-mobile-nav");
+const menu_menu = document.querySelector(".nav-header");
+const menu = document.querySelector(".header");
+const menu_items = document.querySelectorAll(".ul-nav a"); 
+
+
+function closeMenu() {
+  menu.classList.remove("nav-open");
+}
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("active");
+  menu_menu.classList.toggle("active");
+});
+menu_items.forEach(function (item) {
+  item.addEventListener("click", closeMenu);
 });
