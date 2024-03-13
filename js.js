cd39ca465ed67715d429de2a9dd896287eb932b2
matinjamal <162322083+matinjamal@users.noreply.github.com>
2024-03-13 16:54:18 +0430

    Update js.js

--- js.js
+++ js.js
@@ -21,3 +21,31 @@ menu_btn.addEventListener("click", function () {
 menu_items.forEach(function (item) {
   item.addEventListener("click", closeMenu);
 });
+//article
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".service-link");
+  const images = document.querySelectorAll(".port-img");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const target = button.getAttribute("data-target");
+
+      // Remove 'active' class from all buttons
+      buttons.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      // Add 'active' class to the clicked button
+      button.classList.add("active");
+
+      // Show or hide images based on the target
+      images.forEach((image) => {
+        if (target === "all" || image.classList.contains(target)) {
+          image.style.display = "block";
+        } else {
+          image.style.display = "none";
+        }
+      });
+    });
+  });
+});
